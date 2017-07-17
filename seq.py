577b51f474fc9e5687818d110145f29878df0df2
J0J0 T <jojo@peek-a-boo.at>
2017-07-17 07:31:58 +0200

    bytehex, little2dec, track data
      * bytehex shows nicely formated hex values
      * little2dec function converts 2-byte little endian unsigned int to decimal value
      * further details of seq file are shown
      * track data of tracks 1-3 are shown, issue: 3 times the same code
      * issue: probably there is a better approach than iterating through 8 byte chunks!
               maybe read in specific sized chunks e.g "header", "all track data", "rest/data",
               then iterate through those variables, and read up to specific byte numbers

--- seq.py
+++ seq.py
@@ -11,6 +11,13 @@ def hex2dec(s):
   """return the integer value of a hexadecimal string s"""
   return int(s, 16)
 
+def little2dec(byte1, byte2=None):
+  """return the integer value of a little-endian hexadecimal string s"""
+  if byte2==None:
+    return int(binascii.hexlify(byte1[1:2]+byte1[0:1]), 16)
+  else:
+    return int(binascii.hexlify(byte2+byte1), 16)
+
 def chunk2hexgroups(chunk):
   """return 2-byte-groups of binary chunk"""
   hexchunk=binascii.hexlify(chunk)
@@ -88,39 +95,48 @@ for seqfile in os.listdir(PATH):
         chunk = f.read(8) # each chunk is 8 bytes of binary data (\x00\x01...)
         if not chunk:
           break
+        bytedec=chunknr*8
+        bytehex="{0:#0{1}x}".format(bytedec,6)
+        #print '0x%04x' % bytenr # DEBUG OUT ALL HEX CHUNK NUMBERS
+        #print bytehex # DEBUG OUT ALL HEX CHUNK NUMBERS
+        if chunknr==0:
+          print bytehex,"\theader 1:\t\t", chunk
+        if chunknr==1:
+          print bytehex,"\theader 2:\t\t", chunk
         if chunknr==2:
-          print hex(chunknr*8),"\twhat's chunk 2?\t\t", chunk2hexgroups(chunk)
+          print bytehex, "\theader 3:\t\t", chunk
         if chunknr==3:
-          print hex(chunknr*8), "\tbars (and ?) HEX:\t", chunk2hexgroups(chunk)
-          #print hex(chunknr*8), "\tbars HEX reord:\t\t", chunk2hexgroups(chunk[6:7]+chunk[4:5])
-          bars = hex2dec(binascii.hexlify(chunk[6:7]+chunk[4:5]))
-          #bars = ord(chunk[1:2]) << 8 
-          print hex(chunknr*8), "\tbars DEC:\t\t", bars 
+          print bytehex, "\tsome ints and bars HEX:\t", chunk2hexgroups(chunk)
+          someint1 = little2dec(chunk[0:1], chunk[1:2])
+          someint2 = little2dec(chunk[2:3], chunk[3:4])
+          print bytehex, "\tsome ints:\t\t", someint1, " ", someint2  
+          bars = little2dec(chunk[4:5], chunk[6:7]) 
+          print bytehex, "\tbars DEC:\t\t", bars 
         if chunknr==4:
-          print hex(chunknr*8), "\tbpm HEX:\t\t", chunk2hexgroups(chunk)
+          print bytehex, "\tbpm HEX:\t\t", chunk2hexgroups(chunk)
           #tempo_rob = (ord(chunk[1:2]) << 8 | ord(chunk[:1])) / 10
-          #print hex(chunknr*8), "\tbpm DEC Robert:\t\t", tempo_rob
+          #print bytehex, "\tbpm DEC Robert:\t\t", tempo_rob
           #tempo_my_reord = chunk2bytearray(chunk)[1:2]+chunk2bytearray(chunk)[0:1]
-          tempo_my = hex2dec(binascii.hexlify(chunk[1:2]+chunk[0:1])) / 10
-          print hex(chunknr*8), "\tbpm DEC:\t\t", tempo_my
+          tempo_my = little2dec(chunk[0:1], chunk[1:2]) / 10
+          print bytehex, "\tbpm DEC:\t\t", tempo_my
         #if chunk.find('Funk') != -1:
         # in file FunkBG__096ac8ba.SEQ chunk 901 is 2nd part of WAV name!?!
         #if chunknr==901:
-        #  print hex(chunknr*8), "\tchunk 901:\t\t", chunk
+        #  print bytehex, "\tchunk 901:\t\t", chunk
         # Track 1
         if chunknr==903:
-          print hex(chunknr*8), "\tWAV file Tr1 P1:\t", chunk
+          print bytehex, "\tWAV file Tr1 P1:\t", chunk
         if chunknr==905:
-          print hex(chunknr*8), "\tWAV file Tr1 P2:\t", chunk
+          print bytehex, "\tWAV file Tr1 P2:\t", chunk
         #if chunknr==906:
-        #  print hex(chunknr*8), "\tWAV file Tr1 Err:\t\t", binascii.hexlify(chunk)
+        #  print bytehex, "\tWAV file Tr1 Err:\t\t", binascii.hexlify(chunk)
         # Track 2
         #if chunknr==908:
-        #  print hex(chunknr*8), "\tWAV file Tr2 P1:\t", chunk
+        #  print bytehex, "\tWAV file Tr2 P1:\t", chunk
         #if chunknr==909:
-        #  print hex(chunknr*8), "\tWAV file Tr2 P2:\t", chunk
+        #  print bytehex, "\tWAV file Tr2 P2:\t", chunk
         #if chunknr==910:
-        #  print hex(chunknr*8), "\tWAV file Tr2 Err:\t", binascii.hexlify(chunk)
+        #  print bytehex, "\tWAV file Tr2 Err:\t", binascii.hexlify(chunk)
 
         # DEBUG findstr
         #if "070bsD1" in chunk:
@@ -129,7 +145,52 @@ for seqfile in os.listdir(PATH):
         #if "070drTgh" in chunk:
         #  print "chunknr is ", chunknr
         #  print "chunk is ", chunk
+        if bytehex=="0x0028": # actually 0x20f
+          print bytehex, "\tLAST CHUNK BEFORE END OF HEADER (0x20f):"
+        if bytehex=="0x0030":
+          print bytehex, "\tTEMPO MAP STARTS: ", little2dec(chunk[0:2]), little2dec(chunk[2:4]),\
+                                                 little2dec(chunk[4:6]), little2dec(chunk[6:8])
+        if bytehex=="0x0fd0":
+          print bytehex, "\tTRACK DATA STARTS: " 
+          print "\t(64 chunks, 48 bytes each"
+          print "\tname, midich, program, status)"
+        # TRACK 01
+        if bytehex=="0x1000":
+          print bytehex, "\t1st track name:\t\t", chunk[0:8]
+        if bytehex=="0x1010" or bytehex=="0x1018":
+          print bytehex, "\tprogram:\t\t", chunk[0:8]
+        if bytehex=="0x1020":
+          print bytehex, "\tsomething no idea:\t",little2dec(chunk[0:1],chunk[1:2]), \
+                                                  little2dec(chunk[4:7])
+        if bytehex=="0x1028":
+          print bytehex, "\tsome ints & midi ch:\t", little2dec(chunk[0:1], chunk[1:2]), \
+                little2dec(chunk[2:3], chunk[3:4]),  little2dec(chunk[4:5], chunk[5:6]), \
+                chunk[6:7], chunk[7:8]
+        # TRACK 02
+        if bytehex=="0x1030":
+          print bytehex, "\t2nd track name:\t\t", chunk[0:8]
+        if bytehex=="0x1040" or bytehex=="0x1048":
+          print bytehex, "\tprogram:\t\t", chunk[0:8]
+        if bytehex=="0x1050":
+          print bytehex, "\tprogram and something:\t", chunk[0:2], little2dec(chunk[2:4]),\
+                                                  little2dec(chunk[4:7])
+        # TRACK 03
+        if bytehex=="0x1060":
+          print bytehex, "\t3rd track name:\t\t", chunk[0:8]
+        if bytehex=="0x1070" or bytehex=="0x1078":
+          print bytehex, "\tprogram:\t\t", chunk[0:8]
+        if bytehex=="0x1080":
+          print bytehex, "\tsomething no idea:\t",little2dec(chunk[0:1],chunk[1:2]), \
+                                                  little2dec(chunk[4:7])
+        # BOUNDARY
+        if bytehex=="0x1c00":
+          print bytehex, "\tBOUNDARY MARKER:\t", chunk2hexgroups(chunk) 
+        if bytehex=="0x1c10":
+          print bytehex, "\tDATA STARTS: " 
+          print bytehex, "\t(ticks, track#, event): " 
 
+
+        #print hex(chunknr)
         chunknr=chunknr+1
     print ""
 
